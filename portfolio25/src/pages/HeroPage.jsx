371b6ca4f47b8fb3e9cc6fdd7c49a53d2ce6340a
SANJOY <paulsanjoy2923@gmail.com>
2025-07-09 22:55:35 +0530

    home page section done

--- portfolio25/src/pages/HeroPage.jsx
+++ portfolio25/src/pages/HeroPage.jsx
@@ -23,70 +23,78 @@ export const HeroPage = () => {
     }
 
     return (
-        <>
-            <div className='relative h-[100vh] w-[100vw] bg-black overflow-hidden'>
-                {/* Particle Background */}
-                <div className='absolute inset-0 z-0'>
-                    <Particles
-                        particleColors={['#ffffff', '#ffffff']}
-                        particleCount={500}
-                        particleSpread={10}
-                        speed={0.1}
-                        particleBaseSize={100}
-                        moveParticlesOnHover={true}
-                        alphaParticles={false}
-                        disableRotation={true}
-                    />
-                </div>
+        <div className="relative min-h-screen w-[99vw] text-white overflow-x-hidden">
+            {/* Particle Background */}
+            <div className="fixed inset-0 -z-10 bg-black">
+                <Particles
+                    particleColors={['#ffffff', '#ffffff']}
+                    particleCount={500}
+                    particleSpread={10}
+                    speed={0.1}
+                    particleBaseSize={100}
+                    moveParticlesOnHover={true}
+                    alphaParticles={false}
+                    disableRotation={true}
+                />
+            </div>
 
-                {/* Main Hero part */}
-                <div className='absolute inset-0 z-10 flex justify-center items-center text-white'>
-                    <div className='flex flex-row gap-5 items-center text-center'>
-                        <SplineModel />
-                        <div className="flex flex-col gap-5">
-                            <div className='flex flex-col justify-center items-start'>
-                                <p className='text-8xl font-vt323'>
-                                    Hi I am <b className='text-red-500'>Sanjoy</b> :)
-                                </p>
-                                <p className='text-red-500 font-dotgothic text-xl font-semibold'>
-                                    Nice to Meet You All, I am a passionate Web developer and CS undergrad
-                                </p>
-                            </div>
-                            <div className="flex justify-start gap-5">
-                                <Button
-                                    onClick={() => console.log('Clicked!')}
-                                    borderColor="white"
-                                    bg="black"
-                                    textColor="white">
-                                    <p className="flex items-center gap-2 px-5">Contact Me <FaArrowRight /></p>
-                                </Button>
-                                <Button
-                                    onClick={handleCopy}
-                                    borderColor="white"
-                                    bg="black"
-                                    textColor="white">
-                                    <p className="flex items-center gap-2">npx sanjoyxyz <IoMdCopy /></p>
-                                </Button>
-                            </div>
-                            <div>
-                                <Socials />
-                            </div>
+            {/* Main Hero section */}
+            <section className="min-h-screen flex justify-center items-center px-4">
+                <div className="flex flex-col lg:flex-row items-center gap-8 text-center lg:text-left">
+                    <SplineModel />
+                    <div className="flex flex-col gap-5">
+                        <div className="text-left">
+                            <p className="text-8xl font-vt323">
+                                Hi I am <b className="text-red-500">Sanjoy</b> :)
+                            </p>
+                            <p className="text-red-500 font-dotgothic text-xl font-semibold">
+                                Nice to meet you all, I am a passionate Web developer and CS undergrad
+                            </p>
                         </div>
+
+                        <div className="flex gap-5">
+                            <Button
+                                onClick={() => console.log('Clicked!')}
+                                borderColor="white"
+                                bg="black"
+                                textColor="white">
+                                <p className="flex items-center gap-2 px-5">
+                                    Contact Me <FaArrowRight />
+                                </p>
+                            </Button>
+
+                            <Button
+                                onClick={handleCopy}
+                                borderColor="white"
+                                bg="black"
+                                textColor="white">
+                                <p className="flex items-center gap-2">
+                                    npx sanjoyxyz <IoMdCopy />
+                                </p>
+                            </Button>
+                        </div>
+
+                        <Socials />
                     </div>
                 </div>
+            </section>
 
-                {/* Bottom-right alert */}
-                {copy && (
-                    <Bubble
-                        bg="transparent"
-                        borderColor="white"
-                        direction="left"
-                        className="left-88 top-14">
-                        <p className="text-white">Copied to clipboard!</p>
-                        <p className="text-white text-sm">Paste it on terminal to see more :)</p>
-                    </Bubble>
-                )}
-            </div>
-        </>
+            {/* Skills Section */}
+            <section className="h-[60vh] flex justify-center items-center">
+                <h2 className="text-9xl">SKILLS</h2>
+            </section>
+
+            {/* Bottom-right alert */}
+            {copy && (
+                <Bubble
+                    bg="transparent"
+                    borderColor="white"
+                    direction="left"
+                    className="fixed bottom-5 right-5 z-50">
+                    <p className="text-white">Copied to clipboard!</p>
+                    <p className="text-white text-sm">Paste it on terminal to see more :)</p>
+                </Bubble>
+            )}
+        </div>
     )
 }
