5b3fc06be05609d244305b61afa98b000e84f67e
SANJOY <paulsanjoy2923@gmail.com>
2025-07-11 20:09:13 +0530

    Spline Model responsive size done

--- portfolio25/src/components/UI/SplineModel.jsx
+++ portfolio25/src/components/UI/SplineModel.jsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function SplineModel() {
   return (
-    <div className='w-[40vw] h-[60vh]'>
+    <div className='lg:w-[40vw] lg:h-[60vh] h-[60vh] w-[100vw] pointer-events-none'>
       <Spline scene="https://prod.spline.design/4BcwOXWwjxYlPncU/scene.splinecode" />
     </div>
   );
